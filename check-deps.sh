e81cf48304ed563b6b75047e258abfb8fbd8f6cd
areif-dev <ajreifsnyder@protonmail.com>
2024-03-09 16:12:23 -0500

    Initial implementation of arch-gaming

---
+++ check-deps.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash 
+
+dependencies="
+    bc
+    bzip2
+    chpasswd
+    curl
+    diff
+    find
+    findmnt
+    gpg
+    hostname
+    less
+    lsof
+    man
+    mount
+    passwd
+    pigz
+    pinentry
+    ping
+    ps
+    rsync
+    script
+    ssh
+    sudo
+    time
+    tree
+    umount
+    unzip
+    useradd
+    wc
+    wget
+    xauth
+    zip
+"
+for dep in ${dependencies}; do
+    ! command -v "${dep}" && echo "missing $dep"
+done
